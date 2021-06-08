11f9abe7c02fc5f93dd1027386a8ed37336e2fbf
Angela Flowers <me@angela.flowers>
2021-06-08 16:56:57 -0400

    install sqlalchemy

--- config.py
+++ config.py
@@ -3,13 +3,16 @@ import os
 class Config:
     DEBUG = False
     DEVELOPMENT = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
 
 class StagingConfig(Config):
     DEBUG = True
+    DEVELOPMENT = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
